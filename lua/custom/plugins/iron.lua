41ea8d4c18ee11cf9cf9b3fc5218e5c2910ba88f
Ethan Decleyn <ethan.decleyn@mil.be>
2026-07-22 17:44:11 +0200

    Added iron.nvim and made all keymaps prefixed with "<space>r"

--- lua/custom/plugins/iron.lua
+++ lua/custom/plugins/iron.lua
@@ -1,7 +1,4 @@
--- iron
--- https://github.com/Vigemus/iron.nvim
-
-vim.pack.add { 'https://github.com/Vigemus/iron.nvim' }
+vim.pack.add {'https://github.com/Vigemus/iron.nvim'}
 
 local iron = require("iron.core")
 local view = require("iron.view")
@@ -63,22 +60,22 @@ iron.setup {
     -- toggle_repl_with_cmd_1 = "<space>rv",
     -- toggle_repl_with_cmd_2 = "<space>rh",
     restart_repl = "<space>rR", -- calls `IronRestart` to restart the repl
-    send_motion = "<space>sc",
-    visual_send = "<space>sc",
-    send_file = "<space>sf",
-    send_line = "<space>sl",
-    send_paragraph = "<space>sp",
-    send_until_cursor = "<space>su",
-    send_mark = "<space>sm",
-    send_code_block = "<space>sb",
-    send_code_block_and_move = "<space>sn",
-    mark_motion = "<space>mc",
-    mark_visual = "<space>mc",
-    remove_mark = "<space>md",
-    cr = "<space>s<cr>",
-    interrupt = "<space>s<space>",
-    exit = "<space>sq",
-    clear = "<space>cl",
+    send_motion = "<space>rsc",
+    visual_send = "<space>rsc",
+    send_file = "<space>rsf",
+    send_line = "<space>rsl",
+    send_paragraph = "<space>rsp",
+    send_until_cursor = "<space>rsu",
+    send_mark = "<space>rsm",
+    send_code_block = "<space>rsb",
+    send_code_block_and_move = "<space>rsn",
+    mark_motion = "<space>rmc",
+    mark_visual = "<space>rmc",
+    remove_mark = "<space>rmd",
+    cr = "<space>rs<cr>",
+    interrupt = "<space>rs<space>",
+    exit = "<space>rq",
+    clear = "<space>rcl",
   },
   -- If the highlight is on, you can change how it looks
   -- For the available options, check nvim_set_hl
